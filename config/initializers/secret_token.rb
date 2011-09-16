2f6b794288f8ec25719cf1d1b4dfb6195755e72a
unknown <dkahan@.es.fr.alten.com>
2011-09-16 18:05:00 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Link2sol::Application.config.secret_token = '750febbe724b88152f5c3f4f66320e041d71afb5f8ba61ee4be359e1473026979409ef32114b3f9e93b834ab7553f0d5b27070400fdfb28d18548a4aa423afc3'
